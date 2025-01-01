9bb3a3ee3e36a5b144ee45d7f1948f79cd9306ef
nimrakhan <nk1304567@gmail.com>
2025-01-01 02:27:06 -0800

    Update project.py

--- Project/project.py
+++ Project/project.py
@@ -5,7 +5,24 @@ class Student(School):
     pass
 
 class Teacher(School):
-    pass
+    def __init__(self,
+        teacher_name,
+        teacher_f_name,
+        teacher_phone_name,
+        teacher_salary,
+        ):
+        self.teacher_name = teacher_name
+        self.teacher_f_name = teacher_f_name
+        self.teacher_phone_number = teacher_phone_name
+        self.teacher_salary = teacher_salary
+
+    def getteacher(self):
+        return f"""
+    Name: {self.teacher_name} 
+    Father Name: {self.teacher_f_name}
+    Phone Number: {self.teacher_phone_number}
+    Fees: {self.teacher_fees} 
+    """      
 
 class Admin(School):
     pass
