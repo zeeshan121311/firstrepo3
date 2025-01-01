8fcaba5a8a6c85bb9c48b03f799dc9bef3c47a32
zeeshan121311 <zeeshansaeed121125@gmail.com>
2025-01-01 15:15:05 +0500

    Update project.py
    added

--- Project/project.py
+++ Project/project.py
@@ -12,3 +12,20 @@ class Admin(School):
 
 class Classrooms(School):
     pass
+
+# ZEESHAN ALI
+
+class School:
+    pass
+
+class Student(School):
+    pass
+
+class Teacher(School):
+    pass
+
+class Admin(School):
+    pass
+
+class Classrooms(School):
+    pass
