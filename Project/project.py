fffa05ef42c0be5742d9cd3cb32bbe675ea2728a
zeeshan121311 <zeeshansaeed121125@gmail.com>
2025-01-02 15:27:26 +0500

    Update project.py
    Admin Class Added By Zeeshan Ali

--- Project/project.py
+++ Project/project.py
@@ -37,7 +37,14 @@ class Teacher(School):
         return f" Name: {self.teacher_name}  Father Name: {self.teacher_f_name} Phone Number: {self.teacher_phone_number}"   
 
 class Admin(School):
-    pass
+    def __init__(self, FDO, vice_principal, cashier, principal, admin):
+        self.FDO = FDO
+        self.vice_principal = vice_principal
+        self.cashier = cashier
+        self.principal= principal
+        self.admin = admin
+    def getStudent(self):
+        return f"FDO = {self.FDO},vice principal = {self.vice_principal},cashier = {self.cashier},principal = {self.principal},admin = {self.admin},"
 
 class Classrooms(School):
     pass
